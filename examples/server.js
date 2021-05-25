0dbdd89aab9de675bb0540702adce4f58dcc5730
Tong <987717609@qq.com>
2021-05-26 00:42:44 +0800

    Init

---
+++ examples/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+
+const app = express()
+const config = require('./webpack.config.js')
+const compiler = webpack(config)
+
+console.log(config.entry)
+
+// 告知 express 使用 webpack-dev-middleware，
+// 以及将 webpack.config.js 配置文件作为基础配置。
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  })
+)
+
+app.use(webpackHotMiddleware(compiler))
+
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const router = express.Router()
+
+router.get('/simple/get', function (req, res) {
+  res.json({
+    msg: 'hello world'
+  })
+})
+
+app.use(router)
+
+// 将文件 serve 到 port 3000。
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!\n')
+})
