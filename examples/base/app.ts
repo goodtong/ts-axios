bb084c7caca208003063230262525cdd6d68b893
Tong <987717609@qq.com>
2021-05-31 21:37:27 +0800

    Process request body data

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -61,3 +61,20 @@ axios({
     foo: 'bar',
   },
 })
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2,
+  },
+})
+
+const arr = new Int32Array([21, 31])
+
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: arr,
+})
